49a59fcee0ae788152a6c5f0327a3b75cb05398a
minicybersheep <minicybersheep@outlook.com>
2026-01-28 11:11:28 +0800

    初版rotate_activation

--- mojo_opset/backends/ttx/kernels/npu/__init__.py
+++ mojo_opset/backends/ttx/kernels/npu/__init__.py
@@ -18,6 +18,7 @@ from .rmsnorm import rmsnorm_bwd_impl
 from .rmsnorm import rmsnorm_fwd_impl
 from .rmsnorm import rmsnorm_infer_impl
 from .rope import rope_bwd_impl
+from .indexer_rotate_activation import indexer_rotate_activation_impl
 from .rope import rope_fwd_impl
 from .sdpa import sdpa_bwd_impl
 from .sdpa import sdpa_fwd_impl
@@ -46,6 +47,7 @@ __all__ = [
     "rope_fwd_impl",
     "silu_bwd_impl",
     "silu_fwd_impl",
+    "indexer_rotate_activation_impl",
     "swiglu_bwd_impl",
     "swiglu_fwd_impl",
     "sdpa_infer_impl",
